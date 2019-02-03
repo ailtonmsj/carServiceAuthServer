646b05c1353bb4eebd118fd0649d714267e96841
Ailton Muniz Santos Junior <ailton.msj@gmail.com>
2019-02-03 20:32:46 -0200

    Initial commit

---
+++ src/main/java/br/com/amsj/spring/authserver/CarServiceAuthServerApplication.java
@@ -0,0 +1,14 @@
+package br.com.amsj.spring.authserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarServiceAuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarServiceAuthServerApplication.class, args);
+	}
+
+}
+
